59748432e9a273a0cc4c540ce125e8ed7c990250
Nat3y <khalednatey85@gmail.com>
2024-09-23 12:11:23 +0300

    -refactor animation initialization to a function.

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -19,11 +19,7 @@ class _SplashViewBodyState extends State<SplashViewBody>
   @override
   void initState() {
     super.initState();
-    animationController =
-        AnimationController(vsync: this, duration: const Duration(seconds: 1));
-    slideAnimation = Tween<Offset>(begin: const Offset(0, 3), end: Offset.zero).animate(animationController);
-
-    animationController.forward();
+    initSlidingAnimation();
   }
   @override
   void dispose() {
@@ -41,6 +37,13 @@ class _SplashViewBodyState extends State<SplashViewBody>
         SlidingText(slideAnimation: slideAnimation,),
       ],
     );
+  }
+  void initSlidingAnimation() {
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    slideAnimation = Tween<Offset>(begin: const Offset(0, 3), end: Offset.zero).animate(animationController);
+
+    animationController.forward();
   }
 }
 
