87a99e3927706fff08669e6a319f9f9176339fe3
Nat3y <khalednatey85@gmail.com>
2024-09-27 23:29:31 +0300

    -best seller item.

--- lib/features/home/presentaion/views/widgets/home_view_body.dart
+++ lib/features/home/presentaion/views/widgets/home_view_body.dart
@@ -3,6 +3,7 @@ import 'package:bookly_app/core/theming/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import 'best_seller_list_view_item.dart';
 import 'custom_app_bar.dart';
 import 'custom_list_view_item.dart';
 import 'featured_books_list_view.dart';
@@ -20,12 +21,16 @@ class HomeViewBody extends StatelessWidget {
           const CustomAppBar(),
           verticalSpace(30),
           const FeaturedBooksListView(),
-          verticalSpace(16),
+          verticalSpace(20),
           Text("Best Seller", style: AppTheme.font18WhiteBold,),
+          verticalSpace(16),
+          const BestSellerListViewItem(),
         ],
       ),
     );
   }
 }
+
+
 
 
